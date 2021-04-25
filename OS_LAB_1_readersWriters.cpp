1ab61923c7ef484102db4d1fc12513f694b1c7ef
Dekardt <sasha32083@gmail.com>
2021-04-25 11:52:38 +0300

    Change some methods and variables names

--- OS_LAB_1_readersWriters.cpp
+++ OS_LAB_1_readersWriters.cpp
@@ -1,114 +1,118 @@
-//#include <thread>
-//#include <mutex>
-//#include <iostream>
-//#include <string>
-//#include <Windows.h>
-//#include <shared_mutex>
-//
-//using namespace std;
-//
-//#define NUMBER_OF_WRITERS 1
-//#define NUMBER_OF_READERS 11
-//
-//int counter_readers_in_room = 0;
-//
-//// mutex for changing readers counter
-//mutex counter_mutex;
-//
-//// mutex for console print
-//mutex print_mutex;
-//
-//// mutex for reader/writer access
-//shared_mutex enter_section_mutex;
-//
-//void reader_function()
-//{
-//	while (true)
-//	{
-//		// reader can enter even if there are others readers, but not writter
-//		enter_section_mutex.lock_shared();
-//
-//		// increase readers counter and print it
-//		{
-//			counter_mutex.lock();
-//
-//			counter_readers_in_room++;
-//
-//			print_mutex.lock();
-//			cout << "There are " << counter_readers_in_room << " readers in room" << endl;
-//			print_mutex.unlock();
-//
-//			counter_mutex.unlock();
-//		}
-//		
-//		// simulation of readers activity
-//		Sleep(5000 + rand() % 4000);
-//
-//		// decrease readers counter and print it
-//		{
-//			counter_mutex.lock();
-//
-//			counter_readers_in_room--;
-//
-//			print_mutex.lock();
-//			cout << "There are " << counter_readers_in_room << " readers in room" << endl;
-//			print_mutex.unlock();
-//
-//			counter_mutex.unlock();
-//
-//			enter_section_mutex.unlock_shared();
-//		}
-//
-//		// wait befor next iteration
-//		Sleep(3000 + rand() % 2000);
-//	}
-//}
-//
-//void writerFunction()
-//{
-//	while (true)
-//	{
-//		// writer came so readers can enter no more
-//		enter_section_mutex.lock();
-//
-//		print_mutex.lock();
-//		cout << "There is writer in the room" << endl;
-//		print_mutex.unlock();
-//
-//		// simulation of writter activity
-//		Sleep(5000 + rand() % 5000);
-//
-//		// writer left so readers can enter
-//		enter_section_mutex.unlock();
-//
-//		Sleep(1000 + rand() % 1000);
-//	}
-//}
-//
-//void main()
-//{
-//
-//	thread array_of_threads[NUMBER_OF_READERS + NUMBER_OF_WRITERS];
-//
-//	// start threads for readers
-//	for (int i = 0; i < NUMBER_OF_READERS; i++)
-//	{
-//		array_of_threads[i] = thread(reader_function);
-//		Sleep(200);
-//	}
-//
-//	// start threads for writers
-//	for (int i = NUMBER_OF_READERS; i < NUMBER_OF_READERS + NUMBER_OF_WRITERS; i++)
-//	{
-//		array_of_threads[i] = thread(writerFunction);
-//		Sleep(200);
-//	}
-//
-//	for (int i = 0; i < NUMBER_OF_READERS + NUMBER_OF_WRITERS; i++)
-//	{
-//		if (array_of_threads[i].joinable())
-//			array_of_threads[i].join();
-//		else
-//			cout << "Error";
-//	}
-//}
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <shared_mutex>
+
+using namespace std;
+
+#define AMOUNT_OF_WRITERS 1
+#define AMOUNT_OF_READERS 11
+
+int readers_counter = 0;
+
+// mutex for changing readers counter
+mutex counter_mutex;
+
+// mutex for console print
+mutex print_mutex;
+
+// mutex for reader/writer access
+shared_mutex enter_section_mutex;
+
+void reader_function()
+{
+	while (true)
+	{
+		// reader can enter even if there are other readers, but not writer
+		enter_section_mutex.lock_shared();
+
+		// increase readers counter and print it
+		{
+			counter_mutex.lock();
+
+			readers_counter++;
+
+			print_mutex.lock();
+			cout << "There are " << readers_counter << " readers in room" << endl;
+			print_mutex.unlock();
+
+			counter_mutex.unlock();
+		}
+		
+		// simulation of readers activity
+		Sleep(5000 + rand() % 4000);
+
+		// decrease readers counter and print it
+		{
+			counter_mutex.lock();
+
+			readers_counter--;
+
+			print_mutex.lock();
+			cout << "There are " << readers_counter << " readers in room" << endl;
+			print_mutex.unlock();
+
+			counter_mutex.unlock();
+
+			enter_section_mutex.unlock_shared();
+		}
+
+		// wait before next iteration
+		Sleep(3000 + rand() % 2000);
+	}
+}
+
+void writer_function()
+{
+	while (true)
+	{
+		// writer came so readers can enter no more
+		enter_section_mutex.lock();
+
+		print_mutex.lock();
+		cout << "There is writer in the room" << endl;
+		print_mutex.unlock();
+
+		// simulation of writter activity
+		Sleep(5000 + rand() % 5000);
+
+		// writer left so readers can enter
+		enter_section_mutex.unlock();
+
+		Sleep(1000 + rand() % 1000);
+	}
+}
+
+void main()
+{
+
+	thread threads_array[AMOUNT_OF_READERS + AMOUNT_OF_WRITERS];
+
+	// start threads for readers
+	for (int i = 0; i < AMOUNT_OF_READERS; i++)
+	{
+		threads_array[i] = thread(reader_function);
+		Sleep(200);
+	}
+
+	// start threads for writers
+	for (int i = AMOUNT_OF_READERS; i < AMOUNT_OF_READERS + AMOUNT_OF_WRITERS; i++)
+	{
+		threads_array[i] = thread(writer_function);
+		Sleep(200);
+	}
+
+	for (int i = 0; i < AMOUNT_OF_READERS + AMOUNT_OF_WRITERS; i++)
+	{
+		if (threads_array[i].joinable())
+        {
+            threads_array[i].join();
+        }
+		else
+        {
+            cout << "Error";
+        }	
+	}
+}
